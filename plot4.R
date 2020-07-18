cbb02e05d67420e668443ed9242e0f7fa5e51bbf
Srijan Nayak <srijannayak0@gmail.com>
2020-07-18 17:17:24 +0530

    Add plot4 code and png file

---
+++ plot4.R
@@ -0,0 +1,44 @@
+library(readr)
+library(lubridate)
+library(dplyr)
+
+power_consumption <- read_delim(
+  "./data/household_power_consumption.txt",
+  delim = ";",
+  na = "?"
+) %>% mutate(
+  Date = dmy(Date)
+) %>% filter(
+  between(Date, ymd("2007-02-01"), ymd("2007-02-02"))
+) %>% mutate(
+  Time = hms(Time),
+  datetime = Date + Time
+) %>% select(
+  datetime, everything()
+)
+
+png("plot4.png")
+par(mfcol = c(2, 2))
+with(
+  power_consumption,
+  {
+    plot(datetime, Global_active_power,
+         type = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
+    lines(datetime, Global_active_power)
+    
+    plot(datetime, Sub_metering_1,
+         type = "n", xlab = "", ylab = "Energy sub metering")
+    lines(datetime, Sub_metering_1, col = "black")
+    lines(datetime, Sub_metering_2, col = "red")
+    lines(datetime, Sub_metering_3, col = "blue")
+    legend("topright", lty = 1, col = c("black", "red", "blue"),
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    
+    plot(datetime, Voltage, type = "n")
+    lines(datetime, Voltage)
+    
+    plot(datetime, Global_reactive_power, type = "n")
+    lines(datetime, Global_reactive_power)
+  }
+)
+dev.off()
